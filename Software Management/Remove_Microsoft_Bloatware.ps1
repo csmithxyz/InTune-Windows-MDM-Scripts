f5e3dc59349a782657a0744320d8e22f3b3bded1
csmithxyz <58710042+csmithxyz@users.noreply.github.com>
2023-03-22 10:40:32 -0500

    Added Microsoft Accessories Removal

---
+++ Software Management/Remove_Microsoft_Bloatware.ps1
@@ -0,0 +1,53 @@
+# Removing Microsoft Bloatware
+    Get-AppxPackage -name "Microsoft.ZuneMusic" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Music.Preview" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.BingTravel" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.BingHealthAndFitness" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.BingFoodAndDrink" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.People" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.BingFinance" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.3DBuilder" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.BingNews" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.BingSports" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.WindowsCamera" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Getstarted" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Office.OneNote" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.WindowsMaps" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.MicrosoftOfficeHub" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.BingWeather" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.BioEnrollment" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.WindowsStore" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Windows.Photos" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.WindowsPhone" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.GetHelp" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Office.OneNote" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.MicrosoftOfficeHub" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.WindowsFeedbackHub" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.WindowsSoundRecorder" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Messaging" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Print3D" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.ZuneVideo" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.SkypeApp" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.WindowsAlarms" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Microsoft3DViewer" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Windows.ParentalControls" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Paint" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.MSPaint" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Wallet" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.StorePurchaseApp" | Remove-AppxPackage
+
+# Removing Xbox Packages
+    Get-AppxPackage -name "Microsoft.XboxApp" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.XboxGamingOverlay" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Xbox.TCUI" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.XboxGameCallableUI" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.XboxIdentityProvider" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.XboxSpeechToTextOverlay" | Remove-AppxPackage
+
+# Removing Included Games
+    Get-AppxPackage -name "Microsoft.MicrosoftSolitaireCollection" | Remove-AppxPackage
+    Get-AppxPackage -name "king.com.CandyCrushSaga" | Remove-AppxPackage
+    Get-AppxPackage -name "king.com.CandyCrushFriends" | Remove-AppxPackage
+    Get-AppxPackage -name "NORDCURRENT.COOKINGFEVER" | Remove-AppxPackage
+    Get-AppxPackage -name "Microsoft.Advertising.Xaml" | Remove-AppxPackage
+
